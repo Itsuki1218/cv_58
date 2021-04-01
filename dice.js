ca4118b795f26442b84ffc9a939cf0ae496c88a4
Itsuki1218 <78390796+Itsuki1218@users.noreply.github.com>
2021-04-02 06:49:52 +0900

    ファイル変更

---
+++ dice.js
@@ -0,0 +1,6 @@
+const obj = { key: "dadada", age: 123, };
+
+const key1 = 'age';
+
+console.log(obj.key1);   //正しくない
+console.log(obj[key1]);　　//正しい
---
+++ lesson.js
@@ -0,0 +1,31 @@
+console.log("Hello world")
+console.log(123 + 23)
+
+const panch = 'ケンシロウ'
+console.log(panch)
+console.log(`今日は${panch}の出番だ。`)
+
+let i = 0
+i = i + 1
+i = i + 1
+i = i + 1
+i = i + 1
+i = i + 1
+console.log(i)
+
+console.log('aiu\neo')
+
+
+const students = ['西浦', '平野', '瀬崎', '山中', '加藤'];
+const dice = Math.floor(Math.random() * students.length);
+console.log(students[dice]);
+
+const num = Math.random() // 0.0〜1.0の数値をランダムに作る
+
+if (num >= 0.5) {
+    console.log('大きめ')
+} else {
+    console.log('小さめ')
+}
+
+console.log(`数: ${num}`)
